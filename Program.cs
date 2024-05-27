01449f89688cd20e30e8c8076c0249ec5b531ea7
AnaBeneitez <129800955+AnaBeneitez@users.noreply.github.com>
2024-05-27 14:54:28 -0300

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+using HomeBankingMindHub.Models;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<HomeBankingContext>(
+    options => options.UseSqlServer(builder.Configuration.GetConnectionString("HomeBankingConexion"))
+    );
+
+// Add services to the container.
+builder.Services.AddRazorPages();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+}
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapRazorPages();
+
+app.Run();
