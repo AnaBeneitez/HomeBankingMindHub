b3158a9bccabac83228d5c15118eb0a0907c28d7
AnaBeneitez <129800955+AnaBeneitez@users.noreply.github.com>
2024-06-10 10:36:12 -0300

    Minor change in Validations services

--- Services/Implementations/ValidationsService.cs
+++ Services/Implementations/ValidationsService.cs
@@ -14,34 +14,30 @@ namespace HomeBankingMindHub.Services.Implementations
             if (loanApplicationDTO.LoanId == 0 || loanApplicationDTO.Amount == 0 ||
                 loanApplicationDTO.Payments.Equals("0") || loanApplicationDTO.Payments.IsNullOrEmpty() || loanApplicationDTO.ToAccountNumber.IsNullOrEmpty())
             {
-                response = new Response(403, "Faltan campos");
+                 return response = new Response(403, "Faltan campos");
             }
-            else if(loan == null)
+            if(loan == null)
             {
-                response = new Response(403, "El préstamo no existe");
+                return response = new Response(403, "El préstamo no existe");
             }
-            else if(loanApplicationDTO.Amount > loan.MaxAmount)
+            if(loanApplicationDTO.Amount > loan.MaxAmount)
             {
-                response = new Response(403, "El monto solicitado excede el monto máximo");
+                 return response = new Response(403, "El monto solicitado excede el monto máximo");
             }
-            else if (!payments.Contains(loanApplicationDTO.Payments))
+            if (!payments.Contains(loanApplicationDTO.Payments))
             {
-                response = new Response(403, "Las cuotas elegidas no están disponibles para este préstamo");
+                return response = new Response(403, "Las cuotas elegidas no están disponibles para este préstamo");
             }
-            else if(toAccount == null)
+            if(toAccount == null)
             {
-                response = new Response(403, "La cuenta de destino no existe");
+                return response = new Response(403, "La cuenta de destino no existe");
             }
-            else if(toAccount.ClientId != currentId)
+            if(toAccount.ClientId != currentId)
             {
-                response = new Response(403, "La cuenta no pertenece al cliente autenticado");
-            }
-            else
-            {
-                response = new Response(201, "Ok");
+                return response = new Response(403, "La cuenta no pertenece al cliente autenticado");
             }
 
-            return response;
+            return response = new Response(201, "Ok");
         }
 
         public Response MakeTransfer(TransferDTO transferDTO, Account fromAccount, Account toAccount, long currentId)
@@ -51,34 +47,30 @@ namespace HomeBankingMindHub.Services.Implementations
             if (transferDTO.Description.IsNullOrEmpty() || transferDTO.Amount <= 0 ||
                 transferDTO.FromAccountNumber.IsNullOrEmpty() || transferDTO.ToAccountNumber.IsNullOrEmpty())
             {
-                response = new Response(403, "Faltan campos");
+                return response = new Response(403, "Faltan campos");
             }
-            else if (transferDTO.FromAccountNumber.Equals(transferDTO.ToAccountNumber))
+            if (transferDTO.FromAccountNumber.Equals(transferDTO.ToAccountNumber))
             {
-                response = new Response(403, "La cuenta de origen es igual a la de destino");
+                return response = new Response(403, "La cuenta de origen es igual a la de destino");
             }
-            else if (fromAccount == null)
+            if (fromAccount == null)
             {
-                response = new Response(403, "La cuenta de origen no existe");
+                return response = new Response(403, "La cuenta de origen no existe");
             }
-            else if (fromAccount.ClientId != currentId)
+            if (fromAccount.ClientId != currentId)
             {
-                response = new Response(403, "La cuenta de origen no pertenece al cliente autenticado");
+                return response = new Response(403, "La cuenta de origen no pertenece al cliente autenticado");
             }
-            else if (toAccount == null)
+            if (toAccount == null)
             {
-                response = new Response(403, "La cuenta de destino no existe");
+                return response = new Response(403, "La cuenta de destino no existe");
             }
-            else if (fromAccount.Balance < transferDTO.Amount) 
+            if (fromAccount.Balance < transferDTO.Amount) 
             {
-                response = new Response(403, "Saldo insuficiente");
-            }
-            else
-            {
-                response = new Response(201, "Ok");
+                return response = new Response(403, "Saldo insuficiente");
             }
             
-            return response;
+            return response = new Response(201, "Ok");
         }
     }
 }
